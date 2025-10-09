232375a7f8d926b26008642635f6a86b0fc1c2f7
Samuel Meenzen <samuel@meenzen.net>
2025-10-09 15:16:26 +0200

    chore: fix build

--- src/Zammad.Client/Core/ArgumentCheck.cs
+++ src/Zammad.Client/Core/ArgumentCheck.cs
@@ -8,7 +8,7 @@ namespace Zammad.Client.Core
     {
         internal static void ThrowIfNull(object value, string paramName)
         {
-            if (value == null)
+            if (value is null)
             {
                 throw new ArgumentNullException(paramName);
             }
