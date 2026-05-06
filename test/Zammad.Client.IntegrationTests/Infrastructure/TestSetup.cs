04fec8abe918851377974e8cb856a57a50702d9d
Samuel Meenzen <samuel@meenzen.net>
2026-05-06 16:55:11 +0200

    feat: retry search tests

--- test/Zammad.Client.IntegrationTests/Infrastructure/TestSetup.cs
+++ test/Zammad.Client.IntegrationTests/Infrastructure/TestSetup.cs
@@ -5,4 +5,6 @@ public static class TestSetup
     public static string RandomString() => Guid.NewGuid().ToString("N").Substring(0, 8);
 
     public static TimeSpan IndexerDelay => TimeSpan.FromSeconds(5);
+    public const int BackoffMs = 1000;
+    public const int RetryCount = 3;
 }
